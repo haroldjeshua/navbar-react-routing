e850f079d754c68ac1a8d1a5343137241a749c47
Harold Jeshua Varde <harvardii15@gmail.com>
2022-10-25 23:41:44 +0800

    added pages (routing w/o react-router) test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import Navbar from "./Navbar";
+import Home from "./pages/Home"
+import Work from "./pages/Work"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
 
 function App() {
-	return <Navbar/>
+	console.log(window.location)
+
+	switch (window.location.pathname) {
+		case "/":
+			component = <Home />
+			break
+		case "/work":
+			component = <Work />
+			break
+		case "about":
+			component = <About />
+			break
+		case "/contact":
+			component = <Contact />
+			break
+	}
+	return (
+		<>
+			<Navbar/>
+			<div className="container">
+				{component}
+			</div>
+		</>
+	)
 }
 
 export default App;
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,12 +2,17 @@ export default function Navbar() {
 	return <nav className="nav">
 		<a href="/" className="site-title">Site Name</a>
 		<ul>
-			<li>
-				<a href="/pricing">Pricing</a>
-			</li>
-			<li>
-				<a href="/about">About</a>
-			</li>
+			<CustomLink href="/work">Work</CustomLink>
+			<CustomLink href="/about">About</CustomLink>
+			<CustomLink href="/contat">Contact</CustomLink>
 		</ul>
 	</nav>
+}
+
+function CustomLink({ href, children, ...props }) {
+	return (
+		<li>
+			<a href="/work">Work</a>
+		</li>
+	)
 }
